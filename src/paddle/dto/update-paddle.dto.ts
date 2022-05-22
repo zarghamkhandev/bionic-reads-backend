0e0a104a8619aeb12ab3222705dd5c7de7c6d39d
Zargham Khan <zarghamkhan980@gmail.com>
2022-05-22 11:41:38 +0200

    cleaned paddle

--- src/paddle/dto/update-paddle.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePaddleDto } from './create-paddle.dto';
-
-export class UpdatePaddleDto extends PartialType(CreatePaddleDto) {}
--- src/paddle/paddle.service.ts
+++ src/paddle/paddle.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePaddleDto } from './dto/create-paddle.dto';
-import { UpdatePaddleDto } from './dto/update-paddle.dto';
 
 @Injectable()
-export class PaddleService {
-  create(createPaddleDto: CreatePaddleDto) {
-    return 'This action adds a new paddle';
-  }
-
-  findAll() {
-    return `This action returns all paddle`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} paddle`;
-  }
-
-  update(id: number, updatePaddleDto: UpdatePaddleDto) {
-    return `This action updates a #${id} paddle`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} paddle`;
-  }
-}
+export class PaddleService {}
